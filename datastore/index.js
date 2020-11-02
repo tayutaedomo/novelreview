9e4b81bd1d7c1b3c9f9ea4050baf290a4ffa08a3
tayutaedomo <tayutaedomo@gmail.com>
2020-11-02 12:50:47 +0900

    Add store logic to datastore

--- datastore/index.js
+++ datastore/index.js
@@ -1,19 +1,56 @@
+const path = require('path');
+
+const { Datastore } = require('@google-cloud/datastore');
+
+if (process.env['DATASTORE_CREDENTIALS']) {
+  const credentialPath =
+    path.join(__dirname, 'etc', 'google-cloud', process.env['DATASTORE_CREDENTIALS']);
+
+  const datastore = new Datastore({
+    keyFilename: credentialPath
+  });
+
+  const { Gstore, instances } = require('gstore-node');
+  const gstore = new Gstore({
+    cache: false,
+    errorOnEntityNotFound: false,
+  });
+
+  instances.set('default', gstore);
+  gstore.connect(datastore);
+}
+
+const { NovelReviews } = require('novelreview-lib/lib/novel');
+
+
 exports.novelreview_datastore = (req, res) => {
-  try {
-    main(req.body);
+  (async () => {
 
-    res.send('OK');
+    try {
+      main(req.body);
 
-  } catch(err) {
-    console.error(err);
+      res.send('OK');
 
-    res.status(500);
-    res.send('Internal Server Error')
-  }
+    } catch(err) {
+      console.error(err);
+
+      res.status(500);
+      res.send('Internal Server Error')
+    }
+
+  })();
 };
 
 
-const main = (params) => {
-  // TODO
-  console.log(JSON.stringify(params));
+const main = async (params) => {
+  params = params || {};
+
+  if (! params.message || ! params.message.reviews) {
+    console.log('Params are invalid.', JSON.stringify(params));
+    return;
+  }
+
+  const novelReviews = new NovelReviews();
+  await novelReviews.restore(params.message.reviews);
+  await novelReviews.saveModels();
 };
