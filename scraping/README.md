cd9fd8068971109c7635a22a103fbf5dd55e346e
tayutaedomo <tayutaedomo@gmail.com>
2020-11-02 09:49:24 +0900

    Add pubsub publish to scraping

--- scraping/README.md
+++ scraping/README.md
@@ -15,14 +15,22 @@ $ export LINE_POST_USER_ID=<Receiver user id>
 ```
 
 
+## Cloud Pub/Sub
+If you need Pub/Sub, you have to set below env variables.
+```
+$ export PUBSUB_CREDENTIALS=<JSON credential path>
+$ export PUBSUB_TOPIC=<Topic name>
+```
+
+
 ## Cloud Functions
 ### Deploy
 ```
 $ cd novelreview/scraping
-$ gcloud functions deploy novelreview_scraping --region asia-northeast1 --runtime nodejs10 --trigger-http --allow-unauthenticated
+$ gcloud functions deploy novelreview_scraping --region asia-northeast1 --runtime nodejs10 --memory 512 --trigger-http --allow-unauthenticated
 ```
 
-## Local Development
+### Local Development
 ```
 $ cd novelreview/scraping
 $ npm start
--- scraping/index.js
+++ scraping/index.js
@@ -1,55 +1,27 @@
 const puppeteer = require('puppeteer');
-
 const { NovelReviews } = require('novelreview-lib/lib/novel');
 const LINE = require('novelreview-lib/lib/line');
 
+const { PubSub } = require('@google-cloud/pubsub');
+let pubSubClient = null;
+if (process.env['PUBSUB_CREDENTIALS']) {
+  pubSubClient = new PubSub({
+    keyFilename: process.env['PUBSUB_CREDENTIALS']
+  });
+}
+
+
 const LINE_POST_USER_ID = process.env['LINE_POST_USER_ID'];
+const PUBSUB_TOPIC = process.env['PUBSUB_TOPIC'];
 
 
 exports.novelreview_scraping = (req, res) => {
-
-  const createLineMessage = (reviews) => {
-    const messages = reviews.map(payload => {
-      const novel = payload.novel;
-      return [
-        novel.title,
-        [novel.charCount, novel.points, novel.status, novel.genre].join(' / '),
-        novel.lastUpdatedAt,
-        novel.getUrl(),
-      ].join('\n');
-    });
-
-    return messages.join('\n');
-  };
-
   (async () => {
 
     try {
-      const browser = await puppeteer.launch({
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox'
-        ]
-      });
-
-      const page = await browser.newPage();
-
-      const novelReviews = new NovelReviews();
-      const reviews = await novelReviews.scrape(page);
-      const detected = await novelReviews.detect();
-
-      await browser.close();
-
-      const message = createLineMessage(detected);
-
-      if (LINE_POST_USER_ID && message.length > 0) {
-        await LINE.postMessage(LINE_POST_USER_ID, message, true);
-        console.log('Sent message.');
-
-      } else {
-        console.log('Not send message.');
-        console.log(message);
-      }
+      const result = await scraping();
+      await postMessage(result.detected);
+      await publishMessage(result.reviews);
 
       res.send('OK');
 
@@ -61,5 +33,66 @@ exports.novelreview_scraping = (req, res) => {
     }
 
   })();
+};
 
+
+const scraping = async () => {
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  });
+
+  const page = await browser.newPage();
+
+  const novelReviews = new NovelReviews();
+  const reviews = await novelReviews.scrape(page);
+  const detected = await novelReviews.detect();
+
+  await browser.close();
+
+  return { reviews, detected };
+};
+
+const postMessage = async (detected) => {
+  const message = createLineMessage(detected);
+
+  if (LINE_POST_USER_ID && message.length > 0) {
+    await LINE.postMessage(LINE_POST_USER_ID, message, true);
+    console.log('Message posted.');
+
+  } else {
+    console.log('Not posted yet.');
+    console.log(message);
+  }
+};
+
+const createLineMessage = (reviews) => {
+  const messages = reviews.map(review => {
+    const novel = review.novel;
+
+    return [
+      novel.title,
+      [novel.charCount, novel.points, novel.status, novel.genre].join(' / '),
+      novel.lastUpdatedAt,
+      novel.getUrl(),
+    ].join('\n');
+  });
+
+  return messages.join('\n');
+};
+
+const publishMessage = async (reviews) => {
+  const message = JSON.stringify(reviews);
+
+  if (PUBSUB_TOPIC && reviews.length > 0) {
+    const buffer = Buffer.from(message);
+    const messageId = await pubSubClient.topic(PUBSUB_TOPIC).publish(buffer);
+    console.log(`Message published. ${messageId}`);
+
+  } else {
+    console.log('Not published yet.');
+    console.log(message);
+  }
 };
